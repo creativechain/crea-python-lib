14133bd9d165ab3b26ea2f0f8cd0c0f4b2321343
ander7agar <ander7agar@gmail.com>
2018-09-17 20:59:02 +0200

    update graphene lib

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Topic :: Office/Business :: Financial',
     ],
     install_requires=[
-        "crea-graphenelib>=0.1.0",
+        "crea-graphenelib>=0.1.1",
         "websockets>=2.0",
         "scrypt>=0.7.1",
         "diff-match-patch>=20121119",
