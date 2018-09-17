9a52425f936c5dafa8e1a4a56d71a1dbc4be0df8
ander7agar <ander7agar@gmail.com>
2018-09-17 20:40:11 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 setup(
     name='crea-lib',
